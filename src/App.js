34d1a1c05591e2f9f8faa24901936258b882af4b
fazilmohammedn <fazilmohammedbca@gmail.com>
2024-06-01 16:18:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Navbar from './Components/Navbar';
+import Images from './Components/Images';
+import Service from './Components/Services';
+import Steps from './Components/Steps'
+import Review from './Components/Review';
+import Contact from './Components/Contact';
+import About from './Components/About';
+
+import "./style.css"
+import "./style2.css"
+import "./style3.css"
+import "./style4.css"
+import "./style5.css"
+import "./style6.css"
+import "./style7.css"
+function App()
+{
+  return(
+    <>
+<Navbar/>
+<Images/>
+<Service/>
+<Steps/>
+<Review/>
+<Contact/>
+<About/>
+    </>
+  )
+}
+export default (App)
---
+++ src/Components/About.js
@@ -0,0 +1,40 @@
+import one from "../assests/images/one.png"
+function About()
+{
+  return(
+    <div class="about">
+      <div class="about-1">
+      <h1>ABOUT US</h1>
+      </div>
+      <div class="about-container">
+        <div class="about-container-1">
+          <p>SKY TOWERS</p>
+          <p>TEAM OF CONSTRUCTION</p>
+          <p>GET THE APP</p>
+          <p>ABOUT US</p>
+          <p>CONTACT US</p>
+          <p>BETTER WORKERS</p>
+          <p>PUNCIVALITY</p>
+    </div>
+    <div class="about-container-2">
+          <p>CUSTOMER SUPPORT</p>
+          <p>BLOG</p>
+          <p>AFFILIATE</p>
+          <p>PROJECTS</p>
+          <p>INVESTORS</p>
+          <p>TEAMS SUPPORT</p>
+          <p>SAFE TO MONEY</p>
+    </div>
+   
+      </div> 
+
+      <div class="about-container-3">
+    <div class="navbar-1">
+      <h1><img src={one} alt="Home"></img>Sky-Towers</h1>
+    </div>
+    </div>
+    
+    </div>
+  )
+}
+export default (About)
